e57c9fe8158bdde0abd3173f0eb9dc4da10c29b2
Aaditya Taparia <aaditya-taparia@cookpad.com>
2018-02-23 16:25:52 +0900

    show src

--- sf.js
+++ sf.js
@@ -65,8 +65,7 @@ var SF = {
               super();
               const template = link.import.querySelector('template');
               if (template.getAttribute("relative-url") == "true") {
-                let insideHtml = template.innerHTML;
-                console.log(insideHtml);
+                console.log(template.querySelectorAll('[src]'));
               }
               const shadowRoot = this.attachShadow({mode: 'open'})
                 .appendChild(template.content.cloneNode(true));
