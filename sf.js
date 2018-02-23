beb7792a11c57628ed2cc06fe2c5c49f47c28f8f
Aaditya Taparia <aadityataparia@gmail.com>
2018-02-23 16:27:05 +0900

    all img, link

--- sf.js
+++ sf.js
@@ -65,7 +65,8 @@ var SF = {
               super();
               const template = link.import.querySelector('template');
               if (template.getAttribute("relative-url") == "true") {
-                console.log(template.querySelectorAll('* [src]'));
+                console.log(template.querySelectorAll('img'));
+                console.log(template.querySelectorAll('link'));
               }
               const shadowRoot = this.attachShadow({mode: 'open'})
                 .appendChild(template.content.cloneNode(true));
