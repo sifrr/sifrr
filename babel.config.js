007346af7c01bd049335ac788421fdafdebd1195
Aaditya Taparia <aadityataparia@gmail.com>
2018-12-29 15:39:20 +0900

    use babel.config.js instead of .babelrc

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+  const presets = [
+    ['@babel/env', {
+      'targets': {
+        'chrome': 55,
+        'safari': 11,
+        'opera': 42,
+        'firefox': 53
+      },
+      'modules': false
+    }]
+  ];
+  const plugins = [
+    '@babel/plugin-external-helpers'
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
