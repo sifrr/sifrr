13f2fee56b66d300165257b9fd343767a6a33346
Aaditya Taparia <aadityataparia@gmail.com>
2019-01-11 13:43:40 +0900

    add eslint for html

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
+    'mocha': true
+  },
+  'globals': {
+    'ENV': true,
+    'fs': false,
+    'path': false,
+    'chai': false,
+    'assert': false,
+    'expect': false,
+    'should': false,
+    'puppeteer': false,
+    'server': false,
+    'port': false,
+    'PATH': false,
+    'loadBrowser': false,
+    'page': false,
+    'browser': false,
+    'pti': false,
+    'Sifrr': true,
+    'version': true,
+    'exec': true,
+    'seqCMD': true,
+    'SifrrStorage': false
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'sourceType': 'module',
+    'ecmaVersion': 2017,
+    'esversion': 2017
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'warn',
+      'always'
+    ],
+    'quote-props': [
+      'error',
+      'as-needed'
+    ]
+  },
+  'plugins': [
+    'html'
+  ],
+  'settings': {
+    'html/indent': '+2'
+  }
+};
