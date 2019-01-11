26fa6a7d574582c7f7d958c2f910d9e35bb41392
Aaditya Taparia <aadityataparia@gmail.com>
2019-01-11 18:39:57 +0900

    add details

---
+++ details.js
@@ -0,0 +1,26 @@
+module.exports = {
+  '@sifrr/dom': {
+    description: '< 5KB DOM library. If Polymer and react had a better, faster baby.'
+  },
+  '@sifrr/fetch': {
+    description: 'Fetch based http requests library for browsers.'
+  },
+  '@sifrr/route': {
+    description: 'History API based Routing library for building One Page Applications with sifrr-dom.'
+  },
+  '@sifrr/serviceworker': {
+    description: 'Easily customizable Service Worker.'
+  },
+  '@sifrr/storage': {
+    description: 'Browser persisted key-value(JSON) storage library with cow powers.'
+  },
+  '@sifrr/api': {
+    description: 'Library for creating normal apis or GraphQL apis.'
+  },
+  '@sifrr/cli': {
+    description: 'Sifrr Command line interface.'
+  },
+  '@sifrr/seo': {
+    description: 'Middleware to show js generated html to crawler bots with headless chrome.'
+  }
+};
