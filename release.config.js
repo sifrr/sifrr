41af2804af6edb8854d1dcbf777085dcbc6bdc4d
Aaditya Taparia <aadityataparia@gmail.com>
2019-01-15 18:02:43 +0900

    docs: Move scripts and docs to proper folders

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    '@semantic-release/git',
+  ]
+};
