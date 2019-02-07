dc777d6abf401558a6a1396af3f500d8ff1480e6
Aaditya Taparia <aadityataparia@gmail.com>
2019-02-08 06:26:38 +0900

    chore: Add basic benchmarking

---
+++ packages/browser/sifrr-dom/benchmark.js
@@ -0,0 +1,62 @@
+const loadBrowser = require('../../../scripts/test/loadbrowser');
+const exec = require('child_process').exec;
+
+const speedMetrics = {};
+async function runClickBenchmark(name, before, after, metrics = ['LayoutDuration', 'ScriptDuration', 'RecalcStyleDuration'], times = 5) {
+  let totals = {};
+  process.stdout.write(`Running ${name} benchmark ${times} times: \n`);
+  for (let i = 0; i < times; i++) {
+    process.stdout.write(`${i + 1} `);
+
+    // Reload page
+    await page._client.send('Performance.disable');
+    await page.goto(`http://localhost:1111/speedtest.html`);
+    await page.evaluate(async () => await Sifrr.Dom.loading());
+    await page._client.send('Performance.enable');
+
+    // Run before
+    await page.$eval('main-element', (el, before) => el.$(before).click(), before);
+    const beforeMetrics = (await page._client.send('Performance.getMetrics')).metrics;
+
+    // run after
+    await page.$eval('main-element', (el, after) => el.$(after).click(), after);
+    const metrics = (await page._client.send('Performance.getMetrics')).metrics;
+
+    metrics.forEach((m, i) => {
+      totals[m.name] = totals[m.name] || 0;
+      if (beforeMetrics[i].name !== m.name) throw Error('Misconfigured metrics');
+      totals[m.name] += (m.value - beforeMetrics[i].value) * 1000;
+    });
+  }
+  process.stdout.write('\n');
+
+  // Filter metrics
+  for (let m in totals) {
+    if (metrics.indexOf(m) >= 0) {
+      totals[m] = totals[m] / times;
+    } else {
+      delete totals[m];
+    }
+  }
+
+  // Save metrics
+  speedMetrics[name] = totals;
+}
+
+(async () => {
+  await exec('yarn run test-server-only');
+  await loadBrowser();
+
+  await page._client.send('Performance.enable');
+
+  // benchmarks
+  await runClickBenchmark('1k-run', '#clear', '#run');
+  await runClickBenchmark('10k-run', '#clear', '#runlots');
+  await runClickBenchmark('1k-clear', '#run', '#clear');
+  await runClickBenchmark('10k-clear', '#runlots', '#clear');
+  await runClickBenchmark('1k-update10th', '#run', '#update');
+  await runClickBenchmark('10k-update10th', '#runlots', '#update');
+
+  browser.close();
+  global.console.table(speedMetrics);
+})();
---
+++ scripts/test/loadbrowser.js
@@ -0,0 +1,20 @@
+global.puppeteer = require('puppeteer');
+
+module.exports = async function() {
+  // set browser and page global variables
+  let pBrowser = await puppeteer.launch({
+    // to make it work in circleci
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ],
+    headless: process.env.HEADLESS !== 'false',
+    devtools: false
+  });
+  let page = await pBrowser.newPage();
+  await page.setViewport( { width: 1280, height: 800} );
+  global.browser = pBrowser;
+  global.page = page;
+
+  return true;
+};
