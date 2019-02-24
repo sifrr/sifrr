ac38b491ff89e50d10fa395c52b160bec746633d
Aaditya Taparia <aadityataparia@gmail.com>
2019-02-25 00:08:36 +0900

    Make state binding separate (#56)

--- packages/browser/sifrr-dom/benchmark/runclickbenchmark.js
+++ packages/browser/sifrr-dom/benchmark/runclickbenchmark.js
@@ -1,6 +1,6 @@
 const verbose =  Math.max(process.argv.indexOf(`--verbose`), process.argv.indexOf(`-v`)) > 0;
 
-module.exports = async function(benchmark, port, runs = 5, url, warmups = runs, metrics = ['ScriptDuration', 'LayoutCount']) {
+module.exports = async function(benchmark, port, runs = 5, url, warmups = runs, metrics = ['ScriptDuration', 'LayoutCount', 'TaskDuration']) {
   const BM = require(`./benchmarks/${benchmark}`);
   let totals = {};
   if (verbose) process.stdout.write(`Running ${benchmark} benchmark for ${warmups} warmups and ${runs} runs: \n`);
--- packages/browser/sifrr-dom/src/dom/creator.js
+++ packages/browser/sifrr-dom/src/dom/creator.js
@@ -1,26 +1,37 @@
 const { TEXT_NODE, COMMENT_NODE, ELEMENT_NODE, REPEAT_ATTR } = require('./constants');
 const repeatref = require('./repeatref');
 // ref types:
-// 0: text
-// 1: html
-// 2: arrayToDom
-const { getBindingFxns } = require('./bindings');
+// 0: state
+// 1: text
+// 2: html
+// 3: arrayToDom
+const { getBindingFxns, getStringBindingFxn } = require('./bindings');
 
 function customElementCreator(el, filter) {
   if (el.nodeType === TEXT_NODE || el.nodeType === COMMENT_NODE) {
     // text node
     const x = el.data;
-    if (x.indexOf('${') > -1) return {
-      type: 0,
-      text: getBindingFxns(x.trim())
-    };
+    if (x.indexOf('${') > -1) {
+      const binding = getStringBindingFxn(x.trim());
+      if (typeof binding !== 'string') {
+        return {
+          type: 1,
+          text: binding
+        };
+      } else {
+        return {
+          type: 0,
+          text: binding
+        };
+      }
+    }
   } else if (el.nodeType === ELEMENT_NODE) {
     const sm = {};
     // Html ?
     if (filter(el)) {
       const innerHTML = el.innerHTML;
       if (innerHTML.indexOf('${') >= 0) {
-        sm.type = 1;
+        sm.type = 2;
         sm.text = getBindingFxns(innerHTML.replace(/<!--((?:(?!-->).)+)-->/g, '$1').trim());
       }
     } else if (el.hasAttribute(REPEAT_ATTR)) {
