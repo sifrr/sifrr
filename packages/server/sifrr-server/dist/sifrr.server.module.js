cadaa8b8141040c6cfa14daf9332505f903b806a
Aaditya Taparia <aadityataparia@gmail.com>
2019-03-11 22:39:03 +0900

    chore: update sifrr-server rollup

---
+++ packages/server/sifrr-server/dist/sifrr.server.module.js
@@ -0,0 +1,6 @@
+/*! Sifrr.Server v0.0.3 - sifrr project | MIT licensed | https://github.com/sifrr/sifrr */
+var sifrr_server = {};
+
+export default sifrr_server;
+/*! (c) @aadityataparia */
+//# sourceMappingURL=sifrr.server.module.js.map
