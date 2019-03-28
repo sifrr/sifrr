1b74a41d4ae6d89b749a5e154f1767b79509aebd
Aaditya Taparia <aadityataparia@gmail.com>
2019-03-28 13:42:24 +0900

    test(sifrr-server): run ssl tests

--- packages/server/sifrr-server/test/browser/ssl.test.js
+++ packages/server/sifrr-server/test/browser/ssl.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { SPORT } = require('./utils');
 let sapp = require('../public/benchmarks/sifrrssl');
 
-describe.skip('ssl app', function() {
+describe('ssl app', function() {
   before(async () => {
     sapp.listen(SPORT, () => global.console.log('listening sifrr on ', SPORT));
     await page.goto(`https://localhost:${SPORT}/random.html`);
