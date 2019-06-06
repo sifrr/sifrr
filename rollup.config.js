04f4434583dc57e98b442af6e19953672160e146
Aaditya Taparia <aadityataparia@gmail.com>
2019-06-06 17:34:21 +0900

    fix: yarn build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 
-const loadDir = require('./scripts/test/loaddir');
+const { loadDir } = require('@sifrr/dev');
 const allConfigs = [];
 
-loadDir(path.resolve('./packages'), (file) => {
-  if (file.match(/packages\/[a-z]+\/sifrr-[a-z]+\/rollup\.config\.js/)) {
-    Array.prototype.push.apply(allConfigs, require(file));
-  }
-}, 2);
+loadDir({
+  dir: path.resolve('./packages'),
+  onFile: (file) => {
+    if (file.match(/packages\/[a-z]+\/sifrr-[a-z]+\/rollup\.config\.js/)) {
+      Array.prototype.push.apply(allConfigs, require(file));
+    }
+  },
+  deep: 2
+});
 
 module.exports = allConfigs;
