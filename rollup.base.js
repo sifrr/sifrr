3d2b63e88455caa7410af63dfbfbc2f8efd3703e
Aaditya Taparia <aadityataparia@gmail.com>
2019-06-06 22:10:04 +0900

    chore: use @sifrr/dev for rollup config

--- rollup.base.js
+++ rollup.base.js
@@ -1,10 +1,6 @@
 const path = require('path');
 const version = require('./package.json').version;
-const babel = require('rollup-plugin-babel');
-const terser = require('rollup-plugin-terser').terser;
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const cleanup = require('rollup-plugin-cleanup');
+const { getRollupConfig } = require('@sifrr/dev');
 
 const external = [
   'busboy',
@@ -31,48 +27,24 @@ const globals = {
 };
 const footer = '/*! (c) @aadityataparia */';
 
-function moduleConfig(name, root, min = false, isModule = false) {
+function moduleConfig(name, root, minify = false, isModule = false) {
   const filename = name.toLowerCase();
   const banner = `/*! ${name} v${version} - sifrr project | MIT licensed | https://github.com/sifrr/sifrr */`;
-  const ret = {
-    input: path.join(root, `./src/${filename}.js`),
+  return getRollupConfig({
+    name,
+    inputFile: path.join(root, `./src/${filename}.js`),
+    outputFolder: path.join(root, './dist'),
+    outputFileName: filename,
+    minify,
+    type: isModule ? 'module' : 'browser'
+  }, {
     output: {
-      file: path.join(root, `./dist/${filename + (isModule ? '.module' : '') + (min ? '.min' : '')}.js`),
-      format: isModule ? 'es' : 'umd',
-      name: name,
-      banner: banner,
-      footer: footer,
-      sourcemap: !min,
-      preferConst: true,
-      globals: globals
+      banner,
+      footer,
+      globals
     },
-    external: external,
-    plugins: [
-      resolve({
-        browser: !isModule
-      }),
-      commonjs()
-    ]
-  };
-
-  if (!isModule) {
-    ret.plugins.push(babel({
-      exclude: 'node_modules/**',
-      rootMode: 'upward'
-    }));
-  }
-
-  ret.plugins.push(cleanup());
-
-  if (min) {
-    ret.plugins.push(terser({
-      output: {
-        comments: 'all'
-      }
-    }));
-  }
-
-  return ret;
+    external
+  });
 }
 
 module.exports = (name, __dirname, isBrowser = true) => {
