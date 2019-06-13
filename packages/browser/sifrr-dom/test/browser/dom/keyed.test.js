b1744ef4b6c689f31dbc1629dbde52d04e786d7a
Aaditya Taparia <aadityataparia@gmail.com>
2019-06-14 04:47:03 +0900

    test(sifrr-dom): fix keyed tests

--- packages/browser/sifrr-dom/test/browser/dom/keyed.test.js
+++ packages/browser/sifrr-dom/test/browser/dom/keyed.test.js
@@ -9,9 +9,7 @@ function getStates() {
   return ret;
 }
 
-describe('Sifrr.Dom.keyed', function() {
-  this.timeout(0);
-
+describe('Sifrr.Dom.keyed', () => {
   beforeEach(async () => {
     await page.goto(`${PATH}/speedtest.html?useKey`);
     await page.evaluate(async () => { await Sifrr.Dom.loading(); });
@@ -21,14 +19,14 @@ describe('Sifrr.Dom.keyed', function() {
     await page.evaluate(() => document.body.$('main-element').$('#run').click());
     await page.waitForFunction("document.body.$('main-element').$$('tr').length === 1000");
 
-    await page.evaluate(() => window.old2 = document.body.$('main-element').$$('tr')[1]);
-    await page.evaluate(() => window.old998 = document.body.$('main-element').$$('tr')[998]);
+    await page.evaluate("var old2 = document.body.$('main-element').$$('tr')[1]");
+    await page.evaluate("var old998 = document.body.$('main-element').$$('tr')[998]");
 
     await page.evaluate(() => document.body.$('main-element').$('#swaprows').click());
     await page.waitForFunction("document.body.$('main-element').$$('tr')[1].$('td').textContent === '999'");
 
-    const same2 = await page.evaluate(() => window.old2 === document.body.$('main-element').$$('tr')[998]);
-    const same998 = await page.evaluate(() => window.old998 === document.body.$('main-element').$$('tr')[1]);
+    const same2 = await page.evaluate("old2 === document.body.$('main-element').$$('tr')[998]");
+    const same998 = await page.evaluate("old998 === document.body.$('main-element').$$('tr')[1]");
 
     assert(same2);
     assert(same998);
@@ -38,12 +36,12 @@ describe('Sifrr.Dom.keyed', function() {
     await page.evaluate(() => document.body.$('main-element').$('#run').click());
     await page.waitForFunction("document.body.$('main-element').$$('tr').length === 1000");
 
-    await page.evaluate(() => window.old5 = document.body.$('main-element').$$('tr')[5]);
+    await page.evaluate("var old5 = document.body.$('main-element').$$('tr')[5]");
 
     await page.evaluate(() => document.body.$('main-element').$$('tr')[0].$('td .remove').click());
     await page.waitForFunction("document.body.$('main-element').$$('tr')[0].$('td').textContent === '2'");
 
-    const same5 = await page.evaluate(() => window.old5 === document.body.$('main-element').$$('tr')[4]);
+    const same5 = await page.evaluate("old5 === document.body.$('main-element').$$('tr')[4]");
 
     assert(same5);
   });
