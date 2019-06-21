d02360e02252eaa0519c2dcea4312cd54b790c88
Aaditya Taparia <aadityataparia@gmail.com>
2019-06-21 16:24:04 +0900

    fix(sifrr-dom): update value of inputs only if changed

--- packages/browser/sifrr-dom/dist/sifrr.dom.module.js
+++ packages/browser/sifrr-dom/dist/sifrr.dom.module.js
@@ -86,7 +86,7 @@ var template = (str, ...extra) => {
 var updateattribute = (element, name, newValue) => {
   if (newValue === false || newValue === null || newValue === undefined) element.hasAttribute(name) && element.removeAttribute(name);
   else if (name === 'class') element.className = newValue;
-  else if (name === 'id' || name === 'value') element[name] = newValue;
+  else if ((name === 'id' || name === 'value') && element[name] !== newValue) element[name] = newValue;
   else if (element.getAttribute(name) !== newValue) element.setAttribute(name, newValue);
 };
 
--- packages/browser/sifrr-dom/src/dom/updateattribute.js
+++ packages/browser/sifrr-dom/src/dom/updateattribute.js
@@ -2,6 +2,6 @@
 module.exports = (element, name, newValue) => {
   if (newValue === false || newValue === null || newValue === undefined) element.hasAttribute(name) && element.removeAttribute(name);
   else if (name === 'class') element.className = newValue;
-  else if (name === 'id' || name === 'value') element[name] = newValue;
+  else if ((name === 'id' || name === 'value') && element[name] !== newValue) element[name] = newValue;
   else if (element.getAttribute(name) !== newValue) element.setAttribute(name, newValue);
 };
