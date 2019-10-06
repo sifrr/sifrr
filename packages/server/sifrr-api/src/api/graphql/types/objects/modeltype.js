29de3967c9f6b356f4fba2c116a0e1254eeed44b
Aaditya Taparia <aadityataparia@gmail.com>
2019-10-07 03:11:17 +0900

    chore: fix interfaces

--- packages/server/sifrr-api/src/api/graphql/types/objects/modeltype.js
+++ packages/server/sifrr-api/src/api/graphql/types/objects/modeltype.js
@@ -1,4 +1,6 @@
 const ObjectType = require('./objecttype');
+const InterfaceType = require('./interfacetype');
+const Field = require('../field');
 const { objectToMap } = require('../../util');
 
 class ModelType extends ObjectType {
@@ -6,21 +8,12 @@ class ModelType extends ObjectType {
     if (ObjectType.all.get(name)) return ObjectType.all.get(name);
 
     super(name, superOpts);
-    this.interfaces = interfaces;
-  }
-
-  convertInterfaces() {
-    const InterfaceType = require('./interfacetype');
-    this.interfaces = objectToMap(this.interfaces, InterfaceType);
-    this.interfaces.forEach(i => i.fields.forEach((f, name) => this.addField(name, f)));
+    this.interfaces = objectToMap(interfaces, InterfaceType);
+    this.interfaces.forEach(i =>
+      objectToMap(i.fields, Field).forEach((f, name) => this.addField(name, f))
+    );
   }
 }
-
-ModelType.prototype._getSchema = ModelType.prototype.getSchema;
-ModelType.prototype.getSchema = function() {
-  this.convertInterfaces();
-  return this._getSchema();
-};
 
 ModelType.type = 'model';
 
