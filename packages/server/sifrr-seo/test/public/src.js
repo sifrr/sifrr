6a09e8ff7356930ff8c1f067070f1c1b47dba727
Aaditya Taparia <aadityataparia@gmail.com>
2019-11-10 03:46:30 +0900

    chore: fix sifrr-seo test

--- packages/server/sifrr-seo/test/public/src.js
+++ packages/server/sifrr-seo/test/public/src.js
@@ -1,10 +1,8 @@
 import '@webcomponents/shadycss';
 import '@webcomponents/shadydom';
-import SifrrDom from '@sifrr/dom';
 
-window.Sifrr.Dom = SifrrDom;
-Sifrr.Dom.setup({
+window.Sifrr.Dom.setup({
   useShadowRoot: true
 });
-Sifrr.Dom.load('sifrr-test');
-Sifrr.Dom.load('sifrr-nosr');
+window.Sifrr.Dom.load('sifrr-test');
+window.Sifrr.Dom.load('sifrr-nosr');
