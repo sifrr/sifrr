f9f691f29e4a21c6b1b48973d9818b01cfe734eb
Aaditya Taparia <aadityataparia@gmail.com>
2020-01-02 20:21:13 +0900

    chore: add default export

--- packages/browser/sifrr-template/src/sifrr.template.ts
+++ packages/browser/sifrr-template/src/sifrr.template.ts
@@ -1,5 +1,19 @@
+import { default as createUniqueString } from './ustring';
+import { default as createTemplate } from './template/create';
+import { default as update } from './template/update';
+import { default as useFor } from './binders/usefor';
+import { default as useForKeyed } from './binders/useforkeyed';
+
 export { default as createUniqueString } from './ustring';
 export { default as createTemplate } from './template/create';
 export { default as update } from './template/update';
 export { default as useFor } from './binders/usefor';
 export { default as useForKeyed } from './binders/useforkeyed';
+
+export default {
+  createUniqueString,
+  createTemplate,
+  update,
+  useFor,
+  useForKeyed
+};
