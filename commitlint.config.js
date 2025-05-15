379a86b034cd40f7ee2a19fbd6feef3cadad1fe1
Aaditya Taparia <aadityataparia@gmail.com>
2025-05-15 19:25:17 +0900

    feat: new commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,40 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional']
+const fs = require('fs');
+
+const getPackages = () => {
+  const folders = fs
+    .readdirSync('./packages', { withFileTypes: true })
+    .filter((dir) => dir.isDirectory())
+    .map((dir) => dir.name);
+
+  return folders;
 };
+
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)(?:\((.*)\))?!?: (.*)$/,
+      headerCorrespondence: ['type', 'scope', 'subject']
+    }
+  },
+  rules: {
+    'scope-enum': [2, 'always', getPackages()],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'fix',
+        'feat',
+        'chore',
+        'refactor',
+        'test',
+        'revert',
+        // only to be used by release PRs
+        'release'
+      ]
+    ]
+  },
+  plugins: []
+};
+
+module.exports = Configuration;
