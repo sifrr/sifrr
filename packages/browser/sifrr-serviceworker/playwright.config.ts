5e327dced654ebe97c34260c1309c3cf81aee8aa
Aaditya Taparia <aadityataparia@gmail.com>
2025-06-12 19:42:07 +0900

    fix: port

--- packages/browser/sifrr-serviceworker/playwright.config.ts
+++ packages/browser/sifrr-serviceworker/playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from '@playwright/test';
 import * as pkg from '@sifrr/test-suite';
 
-const port = !process.env.CI ? 6007 : 8004;
+const port = !process.env.CI ? 6007 : 8006;
 export default defineConfig(await pkg.getPlaywrightConfigOptions(port));
