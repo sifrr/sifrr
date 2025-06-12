4ad6b715c8ca458a03a49a57656312984a0b4e7f
Aaditya Taparia <aadityataparia@gmail.com>
2025-06-12 20:00:00 +0900

    chore: add release scripts

--- packages/server/sifrr-server/playwright.config.ts
+++ packages/server/sifrr-server/playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from '@playwright/test';
 import * as pkg from '@sifrr/test-suite';
 
-const port = !process.env.CI ? 6006 : 8006;
+const port = !process.env.CI ? 6006 : 8007;
 export default defineConfig(await pkg.getPlaywrightConfigOptions(port));
